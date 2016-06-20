f2b4b2da4970ae3654cfd14c8a062448547290ba
Yan Zhang <emma2740@gmail.com>
2016-06-20 15:57:02 +0200

    Reformat rspec for user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,29 +4,7 @@ RSpec.describe User, type: :model do
   it 'should have valid Factory' do
     expect(FactoryGirl.create(:user)).to be_valid
   end
-
-  it 'is a customer by default' do
-    expect(subject.role).to eq 'customer'
-  end
-
-  it 'can set the role to admin' do
-    admin = FactoryGirl.create(:user, role: 'admin')
-    expect(admin).to be_valid
-  end
-
-  it 'can set the role to restaurant owner' do
-    owner = FactoryGirl.create(:user, role: 'restaurant_owner')
-    expect(owner).to be_valid
-  end
-
-  it 'cannot set the role to x' do
-   # invalid_user = FactoryGirl.create(:user, role: 'x')
-   # expect(invalid_user).not_to be_valid
-
-    expect { FactoryGirl.create(:user, role: 'x') }
-        .to raise_error(ActiveRecord::RecordInvalid, 'Validation failed: Role x is not permitted')
-  end
-
+  
   describe 'DB table' do
     it { is_expected.to have_db_column :id }
     it { is_expected.to have_db_column :username }
@@ -42,4 +20,26 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_uniqueness_of(:username) }
     it { is_expected.to validate_presence_of(:role) }
   end
+
+  describe '#role' do
+    it 'is a customer by default' do
+      expect(subject.role).to eq 'customer'
+    end
+
+    it 'can set the role to admin' do
+      admin = FactoryGirl.create(:user, role: 'admin')
+      expect(admin).to be_valid
+    end
+
+    it 'can set the role to restaurant owner' do
+      owner = FactoryGirl.create(:user, role: 'restaurant_owner')
+      expect(owner).to be_valid
+    end
+
+    it 'cannot set the role to x' do
+      expect { FactoryGirl.create(:user, role: 'x') }
+          .to raise_error(ActiveRecord::RecordInvalid, 'Validation failed: Role x is not permitted')
+    end
+  end
+
 end
