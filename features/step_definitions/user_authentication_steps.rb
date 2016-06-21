1d04ad4fe9d897b5251e6fdb0162ae0cacb9825c
Yan Zhang <emma2740@gmail.com>
2016-06-21 18:48:58 +0200

    Merge with the latest PR 13

--- features/step_definitions/user_authentication_steps.rb
+++ features/step_definitions/user_authentication_steps.rb
@@ -1,20 +1,9 @@
-Given(/^I am on the "([^"]*)"$/) do |page|
-  case page
-    when "sign up page" then
-      visit new_user_registration_path
-  end
-end
-
 Given(/^I fill in "([^"]*)" with "([^"]*)"$/) do |field, value|
   fill_in(field, with: value)
 end
 
 Given(/^I click on the "([^"]*)" button$/) do |button|
   click_button button
-end
-
-Then(/^I should see "([^"]*)"$/) do |text|
-  expect(page).to have_text(text)
 end
 
 Then(/^I should be on the "([^"]*)"$/) do |page|
