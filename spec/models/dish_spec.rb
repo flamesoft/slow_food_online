d5f9ec83c4fd50d7ec590e36c9c43222eb57cfb6
Yan Zhang <emma2740@gmail.com>
2016-06-22 14:14:54 +0200

    Add a test for factorygirl

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Dish, type: :model do
 
+  it 'should have valid Factory' do
+    expect(FactoryGirl.create(:dish)).to be_valid
+  end
+
   describe 'DB table' do
     it { is_expected.to have_db_column :id }
     it { is_expected.to have_db_column :name }
