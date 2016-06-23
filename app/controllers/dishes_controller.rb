befc8ffa5c48f2404dbf49c4888c00e996260f73
Yan Zhang <emma2740@gmail.com>
2016-06-23 12:26:05 +0200

    Comment CRUD methods in dish_controller.rb These methods are not included in the story They will be added for a new story

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -11,39 +11,39 @@ class DishesController < ApplicationController
   end
 
   # GET /dishes/new
-  def new
-    @dish = Dish.new
-  end
+  # def new
+  #   @dish = Dish.new
+  # end
 
   # GET /dishes/1/edit
-  def edit
-  end
+  # def edit
+  # end
 
-  # POST /dishes
-  def create
-    @dish = Dish.new(dish_params)
-
-    if @dish.save
-      redirect_to @dish, notice: 'Dish was successfully created.'
-    else
-      render :new
-    end
-  end
-
-  # PATCH/PUT /dishes/1
-  def update
-    if @dish.update(dish_params)
-      redirect_to @dish, notice: 'Dish was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /dishes/1
-  def destroy
-    @dish.destroy
-    redirect_to dishes_url, notice: 'Dish was successfully destroyed.'
-  end
+  # # POST /dishes
+  # def create
+  #   @dish = Dish.new(dish_params)
+  #
+  #   if @dish.save
+  #     redirect_to @dish, notice: 'Dish was successfully created.'
+  #   else
+  #     render :new
+  #   end
+  # end
+  #
+  # # PATCH/PUT /dishes/1
+  # def update
+  #   if @dish.update(dish_params)
+  #     redirect_to @dish, notice: 'Dish was successfully updated.'
+  #   else
+  #     render :edit
+  #   end
+  # end
+  #
+  # # DELETE /dishes/1
+  # def destroy
+  #   @dish.destroy
+  #   redirect_to dishes_url, notice: 'Dish was successfully destroyed.'
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
