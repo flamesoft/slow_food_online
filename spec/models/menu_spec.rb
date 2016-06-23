a8c4f558fed8ff9ac371628b5620afa7c63983b9
Yan Zhang <emma2740@gmail.com>
2016-06-23 14:25:40 +0200

    Add a test for db columns existence

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Menu, type: :model do
   it 'should have valid Factory' do
     expect(FactoryGirl.create(:menu)).to be_valid
   end
+
+  describe 'DB table' do
+    it { is_expected.to have_db_column :id }
+    it { is_expected.to have_db_column :name }
+  end
 end
