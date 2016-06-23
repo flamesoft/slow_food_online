9650fc03e0812a564a0aa6e983474e43bbfece81
Yan Zhang <emma2740@gmail.com>
2016-06-23 14:24:36 +0200

    Add modle Menu

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623113405) do
+ActiveRecord::Schema.define(version: 20160628083748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,19 @@ ActiveRecord::Schema.define(version: 20160623113405) do
     t.string   "img_url"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "menu_id"
   end
+
+  add_index "dishes", ["menu_id"], name: "index_dishes_on_menu_id", using: :btree
+
+  create_table "menus", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "restaurant_id"
+  end
+
+  add_index "menus", ["restaurant_id"], name: "index_menus_on_restaurant_id", using: :btree
 
   create_table "restaurant_categories", force: :cascade do |t|
     t.string "title"
@@ -68,6 +80,8 @@ ActiveRecord::Schema.define(version: 20160623113405) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "dishes", "menus"
+  add_foreign_key "menus", "restaurants"
   add_foreign_key "restaurants", "restaurant_categories"
   add_foreign_key "restaurants", "users"
 end
