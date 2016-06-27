c6fd9dd9e5f63ff7e0348295f2b7b3ab2ff0ba54
Yan Zhang <emma2740@gmail.com>
2016-06-27 09:49:03 +0200

    Change show details test

--- features/step_definitions/dish_steps.rb
+++ features/step_definitions/dish_steps.rb
@@ -4,6 +4,11 @@ Given(/^the following dishes exist$/) do |table|
   end
 end
 
+Given(/^the dish Dimsun exist$/) do
+  FactoryGirl.create(:dish, name: 'Dimsun', price: '90',
+                     allergy_info: 'soy', calories: '120')
+end
+
 Then(/^I should see all the dishes$/) do
   steps %Q{
     Then I should see "Dimsun"
@@ -23,4 +28,9 @@ end
 Then(/^I should be on the "([^"]*)" for "([^"]*)"$/) do |page, dish_name|
   dish = Dish.find_by(name: dish_name)
   expect(current_path).to eq dish_path(dish)
+end
+
+Given(/^I am on the dish detail page for "([^"]*)"$/) do |dish_name|
+  dish = Dish.find_by(name: dish_name)
+  visit dish_path(dish[:id])
 end
