69530a4431e76f2b9e4647661cfea1644f9afbcf
tochman <thomas@craftacademy.se>
2016-06-28 15:43:18 +0200

    fixes selection of dishes depending on what menu has been chosen

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :restaurants, only: [:show]
+  #get 'restaurant/:id', controller: :restaurants, action: :show
   resources :dishes
   devise_for :users, controllers: { registrations: :registrations }
   root 'landing#index'
