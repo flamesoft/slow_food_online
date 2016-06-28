421cdbd82d2d565caf9fe9778db36ca09793317c
Yan Zhang <emma2740@gmail.com>
2016-06-28 10:43:02 +0200

    Add relationship between Restaurant and Menu including unit test for it

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,7 @@
 class Restaurant < ActiveRecord::Base
   belongs_to :restaurant_category
   belongs_to :user
-
+  has_many :menu
   validates_presence_of :name
   validates_uniqueness_of :name
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160627135559) do
+ActiveRecord::Schema.define(version: 20160628083748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,9 +31,12 @@ ActiveRecord::Schema.define(version: 20160627135559) do
 
   create_table "menus", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "restaurant_id"
   end
+
+  add_index "menus", ["restaurant_id"], name: "index_menus_on_restaurant_id", using: :btree
 
   create_table "restaurant_categories", force: :cascade do |t|
     t.string "title"
@@ -78,6 +81,7 @@ ActiveRecord::Schema.define(version: 20160627135559) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "dishes", "menus"
+  add_foreign_key "menus", "restaurants"
   add_foreign_key "restaurants", "restaurant_categories"
   add_foreign_key "restaurants", "users"
 end
