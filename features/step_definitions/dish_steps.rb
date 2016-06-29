ce3268c79c2565270847a32499988dba6426730c
Yan Zhang <emma2740@gmail.com>
2016-06-29 12:10:18 +0200

    Rename the menus and make the accordingly changes in tests Add menu parameter for dish list page Add soy milk to breakfast menu

--- features/step_definitions/dish_steps.rb
+++ features/step_definitions/dish_steps.rb
@@ -28,16 +28,14 @@ end
 Then(/^I should see all the dishes$/) do
   steps %q{
     Then I should see "Dimsun"
-    Then I should see "Grilled duck"
-    Then I should see "Fried banana"
+    Then I should see "Soy milk"
   }
 end
 
 Then(/^I should see all the prices$/) do
   steps %q{
     Then I should see "80"
-    Then I should see "140"
-    Then I should see "50"
+    Then I should see "40"
   }
 end
 
@@ -49,4 +47,9 @@ end
 Given(/^I am on the dish detail page for "([^"]*)"$/) do |dish_name|
   dish = Dish.find_by(name: dish_name)
   visit dish_path(dish[:id])
+end
+
+Given(/^I am on the "([^"]*)" for menu "([^"]*)"$/) do |page, menu_name|
+  menu = Menu.find_by(name: menu_name)
+  visit dishes_path(menu: menu)
 end
