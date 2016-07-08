af843184788a92244d935032d9e16e98f0f10d40
Yan Zhang <emma2740@gmail.com>
2016-07-08 14:18:11 +0200

    Refactor code - remove useless code

--- spec/requests/api/v1/authentication_spec.rb
+++ spec/requests/api/v1/authentication_spec.rb
@@ -3,18 +3,17 @@ require 'rails_helper'
 RSpec.describe 'Auth', type: :request do
 
   describe 'POST /api/v1/auth/sign_in' do
-
-    let(:headers) { { HTTP_ACCEPT: 'application/json' } }
     let!(:user) { FactoryGirl.create(:user) }
-    let(:credentials) { user.create_new_auth_token }
+
+    before do
+      post '/api/v1/auth/sign_in', { email: user.email, password: user.password }
+    end
 
     it 'valid credentials returns status 200' do
-      post '/api/v1/auth/sign_in', { email: user.email, password: user.password }, headers
       expect(response.status).to eq 200
     end
 
     it 'valid credentials returns correct user' do
-      post '/api/v1/auth/sign_in', { email: user.email, password: user.password }, headers
       expect(response.header['uid']).to eq user.uid
     end
   end
