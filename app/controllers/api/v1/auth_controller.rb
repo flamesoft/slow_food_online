f6f7f96d3485172e680e45ebfee719a4a2f28b65
Yan Zhang <emma2740@gmail.com>
2016-07-08 12:51:34 +0200

    Add test valid credentials returns status 200

--- app/controllers/api/v1/auth_controller.rb
+++
@@ -1,5 +0,0 @@
-class Api::V1::AuthController < ApiController
-  def index
-    render json: { message: 'Welcome' }
-  end
-end
--- spec/requests/api/v1/authentication_spec.rb
+++ spec/requests/api/v1/authentication_spec.rb
@@ -2,15 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'Auth', type: :request do
 
-  describe 'POST /api/v1/auth' do
-    let(:headers) { {HTTP_ACCEPT: 'application/json'} }
-    let!(:customer) { FactoryGirl.create(:user, username: 'test', email: 'test@gmail.com') }
-    let(:credentials) { customer.create_new_auth_token }
+  describe 'POST /api/v1/auth/sign_in' do
 
-    it 'is able to sign in' do
-      post '/api/v1/auth', {email: 'test@gmail.com',
-                            password: 'password'}, headers.merge!(credentials)
-      expect(response_json['status']).to eq('Welcome')
+    let(:headers) { {HTTP_ACCEPT: 'application/json'} }
+    let!(:user) { FactoryGirl.create(:user) }
+    let(:credentials) { user.create_new_auth_token }
+
+    it 'valid credentials returns status 200' do
+      post '/api/v1/auth/sign_in', {email: user.email, password: user.password}, headers
       expect(response.status).to eq 200
     end
   end
