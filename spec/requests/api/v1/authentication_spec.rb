a74a7cac0534b53c04012a028109e0303cc51dda
Yan Zhang <emma2740@gmail.com>
2016-07-08 12:56:22 +0200

    Refactor code on format

--- spec/requests/api/v1/authentication_spec.rb
+++ spec/requests/api/v1/authentication_spec.rb
@@ -4,17 +4,17 @@ RSpec.describe 'Auth', type: :request do
 
   describe 'POST /api/v1/auth/sign_in' do
 
-    let(:headers) { {HTTP_ACCEPT: 'application/json'} }
+    let(:headers) { { HTTP_ACCEPT: 'application/json' } }
     let!(:user) { FactoryGirl.create(:user) }
     let(:credentials) { user.create_new_auth_token }
 
     it 'valid credentials returns status 200' do
-      post '/api/v1/auth/sign_in', {email: user.email, password: user.password}, headers
+      post '/api/v1/auth/sign_in', { email: user.email, password: user.password }, headers
       expect(response.status).to eq 200
     end
 
     it 'valid credentials returns correct user' do
-      post '/api/v1/auth/sign_in', {email: user.email, password: user.password}, headers
+      post '/api/v1/auth/sign_in', { email: user.email, password: user.password }, headers
       expect(response.header['uid']).to eq user.uid
     end
   end
