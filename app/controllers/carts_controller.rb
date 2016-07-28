56a1461261847b4e576e65f0769d56f53c2ca7fd
Carl-Philip <callea2@gmail.com>
2016-07-28 14:58:47 +0200

    Refactored

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,9 +1,6 @@
 class CartsController < ApplicationController
   def show
-    current_order = Order.find_by(user: current_user)
-    @order_subtotal = current_order.subtotal
-    @order_taxes = current_order.taxes
-    @order_total = current_order.total
-    @order_items = current_order.shopping_cart_items.map(&:item)
+    @current_order = Order.find_by(user: current_user)
+    @order_items = @current_order.shopping_cart_items.map(&:item)
   end
 end
